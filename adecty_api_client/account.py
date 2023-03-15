4078cf0a47ce17e71c7675f61891e3cd86e9e55d
Yegor Yakubovich <personal@yegoryakubovich.by>
2023-03-15 06:39:51 +0300

    Methods for working with an account

---
+++ adecty_api_client/account.py
@@ -0,0 +1,42 @@
+#
+# (c) 2022, Yegor Yakubovich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from adecty_api_client.functions import request_create
+
+
+class Account:
+    def create(self, username: str, password: str):
+        endpoint = 'account/create'
+        data = {
+            'username': username,
+            'password': password,
+        }
+        return request_create(
+            endpoint=endpoint,
+            data=data,
+        )
+
+    def session_create(self, username: str, password: str):
+        endpoint = 'account/session/create'
+        data = {
+            'username': username,
+            'password': password,
+        }
+        return request_create(
+            endpoint=endpoint,
+            data=data,
+        )
